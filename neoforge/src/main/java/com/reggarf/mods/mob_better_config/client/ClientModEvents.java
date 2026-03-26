363668b62768f91c7e9c5d0f4e2d1523a35283ee
Anoop Singh <Reggerfgod@gmail.com>
2026-03-26 18:23:33 +0530

    1.21.5

--- neoforge/src/main/java/com/reggarf/mods/mob_better_config/client/ClientModEvents.java
+++ neoforge/src/main/java/com/reggarf/mods/mob_better_config/client/ClientModEvents.java
@@ -10,11 +10,7 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
-@EventBusSubscriber(
-        modid = Constants.MOD_ID,
-        bus = EventBusSubscriber.Bus.MOD,
-        value = Dist.CLIENT
-)
+@EventBusSubscriber(modid = Constants.MOD_ID, value = Dist.CLIENT)
 public class ClientModEvents {
 
     @SubscribeEvent
