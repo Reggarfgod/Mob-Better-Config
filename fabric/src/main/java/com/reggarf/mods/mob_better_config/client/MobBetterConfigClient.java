d669219efb1519070ebbb07595662ee6c7ef34a2
Anoop Singh <Reggerfgod@gmail.com>
2026-04-01 00:50:06 +0530

    Fix

--- fabric/src/main/java/com/reggarf/mods/mob_better_config/client/MobBetterConfigClient.java
+++ fabric/src/main/java/com/reggarf/mods/mob_better_config/client/MobBetterConfigClient.java
@@ -1,6 +1,6 @@
 package com.reggarf.mods.mob_better_config.client;
 
-import com.reggarf.mods.mob_better_config.api.BetterMessages;
+
 import net.fabricmc.api.ClientModInitializer;
 
 public class MobBetterConfigClient implements ClientModInitializer {
